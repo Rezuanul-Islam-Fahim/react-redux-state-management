d3141937ef67bb3d7c5fa3f56e21e794d6b5cefa
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-20 17:33:40 +0600

    Create Dashboard screen

--- src/features/Dashboard/Dashboard.jsx
+++ src/features/Dashboard/Dashboard.jsx
@@ -1,6 +1,15 @@
+import { Link } from 'react-router'
+
 const Dashboard = () => {
     return (
-        <h1>This is Dashboard</h1>
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="flex flex-col">
+                <Link
+                    className="btn btn-primary my-2">
+                    Counter App
+                </Link>
+            </div>
+        </div>
     )
 }
 

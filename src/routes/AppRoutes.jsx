df14d33f2f36ce512b7172ffa00b272a0aedd288
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-22 00:01:04 +0600

    Remove dashboard screen

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router';
-import Dashboard from '@/features/Dashboard';
 import { Counter } from '@/features/Counter';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route index element={<Dashboard />} />
-      <Route path="/counter-app" element={<Counter />} />
+      <Route index element={<Counter />} />
     </Routes>
   );
 };
